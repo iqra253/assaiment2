16aa495316591849a84df0927ffb0afc0131ba7e
iqra253 <iqrayounas253@gmail.com>
2022-09-06 19:49:18 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const shoppingCart = [
+    {
+        product: 'cart',
+        price: 6.88,
+        quantity: 1,
+    },
+    {
+        product: 'shoes',
+        price: 6.88,
+        quantity: 2,
+    },
+    {
+        product: 'books',
+        price: 6.88,
+        quantity: 3,
+    }
+]
+console.log(...shoppingCart);
+
+const student = {
+    firstName: 'baby',
+    lastName: 'doll',
+    strengths: ['Music', 'Art'],
+    exams: {
+        midterm: 92,
+        final:88
+    }
+}
+console.log(...student);
